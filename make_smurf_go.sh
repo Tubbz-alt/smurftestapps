1a0eaac07186e35f13cea718cc6f0c69751773fe
Kukhee Kim <khkim@slac.stanford.edu>
2018-11-02 20:18:16 -0700

    tweak to test app

--- make_smurf_go.sh
+++ make_smurf_go.sh
@@ -4,7 +4,7 @@ caput test_epics:AMCc:FpgaTopLevel:AppTop:AppCore:SysgenCryo:Base[2]:streamEnabl
 caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AxiSy56040:OutputConfig[1] 0
 caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AmcCarrierTiming:EvrV2CoreTriggers:EvrV2ChannelReg[0]:Enable 1
 caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AmcCarrierTiming:EvrV2CoreTriggers:EvrV2ChannelReg[0]:DestSel 0x20000
-caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AmcCarrierTiming:EvrV2CoreTriggers:EvrV2ChannelReg[0]:RateSel 5
+caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AmcCarrierTiming:EvrV2CoreTriggers:EvrV2ChannelReg[0]:RateSel 6
 caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AmcCarrierTiming:EvrV2CoreTriggers:EvrV2TriggerReg[0]:Enable 1
 caput test_epics:AMCc:FpgaTopLevel:AmcCarrierCore:AmcCarrierTiming:EvrV2CoreTriggers:EvrV2TriggerReg[0]:Width 10
 
