6b35988ff54c0edb8b37a7dd3d1fdb5da0657779
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-03-15 15:51:48 -0700

    Moved over from smurf2mce because I think it makes more sense to be here.

---
+++ soft_startup.sh
@@ -0,0 +1,7 @@
+## 9d50d1a0
+./smurf_run.sh -m 10.0.1.4 -c 3 -s 2 -t /home/cryo/shawn/fw_tests/lbhb_fw_test4/MicrowaveMuxBpEthGen2-0x00000001-20190313104109-mdewart-9d50d1a0.pyrogue.tar.gz -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -r test_epics2 -i
+
+## ed07dde0
+#./smurf_run.sh -m 10.0.1.4 -c 3 -s 2 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -r test_epics2 -i
+
+#./smurf_run.sh -m 10.0.1.4 -c 3 -s 5 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults_hb.yml -r test_epics5
---
+++ startup.sh
@@ -0,0 +1,7 @@
+## 9d50d1a0
+joe_hammer -s 2 -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -t /home/cryo/shawn/fw_tests/lbhb_fw_test4/MicrowaveMuxBpEthGen2-0x00000001-20190313104109-mdewart-9d50d1a0.pyrogue.tar.gz -e test_epics2
+
+## ed07dde0
+#joe_hammer -s 2 -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -e test_epics2
+
+#joe_hammer -s 5 -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults_hb.yml -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -e test_epics5
